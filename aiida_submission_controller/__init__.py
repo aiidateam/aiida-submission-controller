cd177cac0d009e41a5536b38b1d9ea571c09f300
Marnik Bercx <mbercx@gmail.com>
2025-01-10 21:56:46 +0100

    🚀 Release `v0.3.0`

--- aiida_submission_controller/__init__.py
+++ aiida_submission_controller/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """A prototype package with tools to submit processes in batches, avoiding to submit too many."""
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __author__ = "Giovanni Pizzi, Austin Zadoks"
 
